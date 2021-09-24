88282169c0ea04478f9bd6e813a64d447833cd2e
thaipham05 <91303688+thaipham05@users.noreply.github.com>
2021-09-24 11:56:55 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {  
+     
+     printf("khi anh qua thung lung\n");
+     printf("va bong dem ghi ban chan\n");
+     printf("doi khien anh chang con nhieu luyen luu\n");
+     printf("anh mong lau mat em kho\n");
+    
+	return 0;
+}
